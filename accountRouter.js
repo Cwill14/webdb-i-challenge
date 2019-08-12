f45811095645775eba23671bfc3359a8cb3ed0e3
Clark Williams <theclarkwilliams@gmail.com>
2019-08-12 15:52:24 -0600

    converted if/else statements into short-circuit conditional and ternaries, cleaned up old code

--- accountRouter.js
+++ accountRouter.js
@@ -4,34 +4,24 @@ const db = require('./data/dbConfig.js');
 const router = express.Router();
 
 function validateAccount(req, res, next) {
-    // if (req.body.name) {
-    //     if (req.body.budget) {
-    //         next();
-    //     } else {
-    //         res.status(400).json({ error: "please provide budget" })
-    //     }
-    // } else {
-    //     res.status(400).json({ error: "please provide name" })
-    // }
     if (req.body) {
         next();
     } else {
         res.status(400).json({ error: "please provide and/or name and budget"})
     }
 }
+
 router.get('/', (req, res) => {
     const { limit, sortby, sortdir } = req.query;
     const query = db('accounts');
-    if (limit) {
-        query.limit(limit);
-    }
-    if (sortby) {
-        if (sortdir) {
-            query.orderBy(sortby, sortdir);
-        } else {
-            query.orderBy(sortby);
-        }
-    }
+
+    limit && query.limit(limit);
+    sortby
+        ? sortdir
+        ? query.orderBy(sortby, sortdir)
+        : query.orderBy(sortby)
+        : null
+
     query
         .then(accounts => {
             res.status(200).json(accounts);
@@ -56,25 +46,22 @@ router.post('/', validateAccount, (req, res) => {
     const post = req.body;
     db('accounts').insert(post, 'id')
         .then(account => {
-            res.status(201).json(account)
+            res.status(201).json({ "new account id": account })
         })
         .catch(err => {
             res.status(500).json({ error: 'error adding account' })
         })
 });
 
-// router.put('/accounts/:id', validateAccount, validateUpdateId, (req, res) => {
 router.put('/:id', validateAccount, (req, res) => {
     const changes = req.body;
     db('accounts')
         .where('id', '=', req.params.id)
         .update(changes)
             .then(count => {
-                if (count > 0) {
-                    res.status(200).json(count)
-                } else {
-                    res.status(404).json({ error: "id not found" })
-                }
+                count > 0 
+                ? res.status(200).json({ "accounts updated": count })
+                : res.status(404).json({ error: "id not found" })
             })
             .catch(err => {
                 res.status(500).json({ error: "error updating account" })
@@ -86,11 +73,9 @@ router.delete('/:id', (req, res) => {
         .where('id', '=', req.params.id)
         .del()
             .then(count => {
-                if (count > 0) {
-                    res.status(200).json(count)
-                } else {
-                    res.status(404).json({ error: "id not found" })
-                }
+                count > 0 
+                ? res.status(200).json({ "accounts deleted": count })
+                : res.status(404).json({ error: "id not found" })
             })
             .catch(err => {
                 res.status(500).json({ error: "error deleting account" })
