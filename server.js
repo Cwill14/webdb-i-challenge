8ccc51658b66012b7cbf61fc2a4c09a874a48cdb
Clark Williams <theclarkwilliams@gmail.com>
2019-08-12 13:45:01 -0600

    answered query questions (except stretch for now) in queries.md, server up and running

--- server.js
+++ server.js
@@ -5,5 +5,18 @@ const db = require('./data/dbConfig.js');
 const server = express();
 
 server.use(express.json());
+server.use(logger);
+
+function logger(req, res, next) {
+    const method = req.method;
+    const url = req.url;
+    const timestamp = Date.now();
+    console.log(`${method} request to '${url}' at ${timestamp}`);
+    next()
+};
+
+server.get('/', (req, res) => {
+    res.status(200).json({ message: "it's working!" })
+});
 
 module.exports = server;
